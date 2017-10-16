a0c7bdf1be3dedafee7ff4cb5d1040905b94d7b9
Jakub Štefanišin <xstefa22@stud.fit.vutbr.cz>
2017-10-16 22:30:56 +0200

    Hash table + structures to save variable/function data

---
+++ symtable.h
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include "symbol.h"
+
+
+typedef struct htab_listitem htab_listitem;
+typedef struct htab_t htab_t;
+
+typedef union 
+{
+	type_variable;
+	type_function;
+} item_type;
+
+struct htab_listitem
+{
+	htab_listitem *next;
+	item_type type;
+	union
+	{
+		struct variable_data *variable;
+		struct function_data *function;
+	} pointer;
+	char *key;
+};
+
+struct htab_t
+{
+	unsigned size;
+	htab_listitem *list[];
+};
+
+
+unsigned hash_function(const char *str, unsigned size);
+
+htab_t *htab_init(unsigned size);
+
+htab_t *htab_copy(htab_t *table);
+
+htab_listitem *htab_add(htab_t *table, const char *key);
+
+htab_listitem *htab_find(htab_t *table, const char *key);
+
+void htab_foreach(htab_t *table, void (*function)(const char *key, item_type type, htab_listitem *item));
+
+void htab_remove(htab_t *table, const char *key);
+
+void htab_clear(htab_t *table);
+
+void htab_free(htab_t *table);
