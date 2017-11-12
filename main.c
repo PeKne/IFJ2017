80bbc15b74bc29482931296228343a7a4eda9645
Petr Marek <petr.marek18@gmail.com>
2017-11-12 11:41:26 +0100

    Zmena nazvu funkce str_add_char na str_push_char!!

--- main.c
+++ main.c
@@ -10,14 +10,30 @@ int main()
     if ((err = str_create(&(token.t_str))) != 0)
         return err;
     
-    for (int i = 0; i < 10; i++)
+    /*for (int i = 0; i < 100; i++)
     {   
         err = generate_token();
         if (token.t_state == st_eof)
             return err;
-        printf("token: %d, error: %d\n", token.t_state, err);
-        printf("str:'%s' delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
+        
+            printf("%d) token: '%d', str: '%s'\n", i, token.t_state, token.t_str.data);
+            //printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
+    }*/
+    for (int i = 0; i < 30; ++i)
+    {
+        str_push_char(&(token.t_str), 'a');
+    
     }
+    printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
+
+    for (int i = 0; i < 30; ++i)
+    {
+        str_pop_char(&(token.t_str));
+        printf("str:%s delka:%d vel.:%d\n", token.t_str.data, token.t_str.length, token.t_str.size);
+    }
+    
+    
+
     str_destroy(&(token.t_str));
 
     return err;
