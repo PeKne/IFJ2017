5c263a7ae2de213a58e9e1eaad3d064ae95f4bcb
Petr Marek <petr.marek18@gmail.com>
2017-11-13 10:03:30 +0100

    Takhle je to nejlepší.

--- main.c
+++ main.c
@@ -10,13 +10,15 @@ int main()
     if ((error = str_create(&(token.t_str))) != 0)
         return error;
     
-for (int i = 0; token.t_state != st_eof; i++)
+for (int i = 0;token.t_state != st_eof; i++)
 {
     error = generate_token();
-    if (token.t_state != st_eof && (error == 0)) {
-        printf("%d) token: '%d', str: '%s'\n", i, token.t_state, token.t_str.data);
+    if (error)
+        break;
+
+    printf("%d) token: '%d', str: '%s'\n", i, token.t_state, token.t_str.data);
         //printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
-    }
+
 }
 
     str_destroy(&(token.t_str));
