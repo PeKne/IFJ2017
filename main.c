ae2d25dba5e0fdae1af7a653fc1d731592c58fba
Petr Marek <petr.marek18@gmail.com>
2017-11-13 10:13:13 +0100

    Odsazeni

--- main.c
+++ main.c
@@ -10,20 +10,20 @@ int main()
     if ((error = str_create(&(token.t_str))) != 0)
         return error;
     
-while (1)
-{
-    error = generate_token();
-    if (error) {
-        break;
-    } else if (token.t_state == st_eof) {
-        printf("EOF\n");
-        break;
+    while (1)
+    {
+        error = generate_token();
+        if (error) {
+            break;
+        } else if (token.t_state == st_eof) {
+            printf("EOF\n");
+            break;
+        }
+
+        printf("token: '%d', str: '%s'\n", token.t_state, token.t_str.data);
+            //printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
+
     }
-
-    printf("token: '%d', str: '%s'\n", token.t_state, token.t_str.data);
-        //printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
-
-}
 
     str_destroy(&(token.t_str));
 
