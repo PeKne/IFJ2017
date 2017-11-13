e44c17bc779373664c9e9bad6cb57eb797530f67
Petr Marek <petr.marek18@gmail.com>
2017-11-13 09:52:35 +0100

    Main projede cely soubor

--- main.c
+++ main.c
@@ -10,19 +10,14 @@ int main()
     if ((error = str_create(&(token.t_str))) != 0)
         return error;
     
-for (int i = 0; i < 4; ++i)
+for (int i = 0; token.t_state != st_eof; i++)
 {
-        error = generate_token();
-    if (token.t_state == st_eof) {
-        printf("EOF\n");
-    }
-    if (error == 0) {
-        printf("token: '%d', str: '%s'\n", token.t_state, token.t_str.data);
+    error = generate_token();
+    if (token.t_state != st_eof && (error == 0)) {
+        printf("%d) token: '%d', str: '%s'\n", i, token.t_state, token.t_str.data);
         //printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
     }
 }
-
-
 
     str_destroy(&(token.t_str));
 
