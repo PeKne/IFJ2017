375b7b98c75f2f8778ae716d1657ceff7cb2cf2f
Petr Marek <petr.marek18@gmail.com>
2017-11-13 10:10:36 +0100

    Rozlišení error a EOF.

--- main.c
+++ main.c
@@ -10,11 +10,15 @@ int main()
     if ((error = str_create(&(token.t_str))) != 0)
         return error;
     
-while (token.t_state != st_eof)
+while (1)
 {
     error = generate_token();
-    if (error)
+    if (error) {
         break;
+    } else if (token.t_state == st_eof) {
+        printf("EOF\n");
+        break;
+    }
 
     printf("token: '%d', str: '%s'\n", token.t_state, token.t_str.data);
         //printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
