747969d83531ebb104b1f1e26e25c3c736d02cd6
Petr Marek <petr.marek18@gmail.com>
2017-11-13 09:21:48 +0100

    Token získá stav 'string' hned po přečtení znaků ' !" '.Main vypisuje vic tokenů.

--- main.c
+++ main.c
@@ -10,8 +10,9 @@ int main()
     if ((error = str_create(&(token.t_str))) != 0)
         return error;
     
-
-    error = generate_token();
+for (int i = 0; i < 4; ++i)
+{
+        error = generate_token();
     if (token.t_state == st_eof) {
         printf("EOF\n");
     }
@@ -19,6 +20,9 @@ int main()
         printf("token: '%d', str: '%s'\n", token.t_state, token.t_str.data);
         //printf("str:%s delka:%d vel.:%d\n\n", token.t_str.data, token.t_str.length, token.t_str.size);
     }
+}
+
+
 
     str_destroy(&(token.t_str));
 
