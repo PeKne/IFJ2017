bab851da6f546545b8d0e31be138dfb9dd50e095
Petr Marek <petr.marek18@gmail.com>
2017-11-27 10:52:42 +0100

    Popis použití debug_print

--- errors.h
+++ errors.h
@@ -23,4 +23,8 @@
 #define debug_print(fmt, ...) \
         do { if (DEBUG_TEST) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
                                 __LINE__, __func__, __VA_ARGS__); } while (0)
+
+/* Pouzití: Nad #include "errors.h" #define DUBUG
+   Výpisy:  debug_print("%s\n", "Popis chyby");
+
 #endif
